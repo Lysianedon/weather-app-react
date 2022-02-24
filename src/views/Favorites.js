d46941a650b47558b9184cd33a09c8257d9480bd
lysiandon <don.lysiane@gmail.com>
2022-02-24 12:57:46 +0100

    created component favorite card

--- src/views/Favorites.js
+++ src/views/Favorites.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useState, useEffect, useContext } from "react";
+
+
 
 export default function Favorites() {
   return (
-    <div>Favorites</div>
+    <div>
+
+      <h1>FAVORITES</h1>
+
+      <h3></h3>
+      <h3></h3>
+      <h3></h3>
+      <h3></h3>
+
+
+
+    </div>
   )
 }
